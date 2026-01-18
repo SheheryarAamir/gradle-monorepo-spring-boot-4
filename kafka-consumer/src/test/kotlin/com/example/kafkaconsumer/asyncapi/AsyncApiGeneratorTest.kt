91a81590290579cc85a00850a253da541a89d248
sheheryar aamir <sheheryaramir@gmail.com>
2026-01-18 12:51:28 +0100

    feat: update ci with docs verification check

--- kafka-consumer/src/test/kotlin/com/example/kafkaconsumer/asyncapi/AsyncApiGeneratorTest.kt
+++ kafka-consumer/src/test/kotlin/com/example/kafkaconsumer/asyncapi/AsyncApiGeneratorTest.kt
@@ -5,8 +5,24 @@ import io.github.springwolf.asyncapi.v3.jackson.DefaultAsyncApiSerializerService
 import io.github.springwolf.core.asyncapi.AsyncApiService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 
+@EmbeddedKafka(
+    partitions = 1,
+    bootstrapServersProperty = "spring.kafka.bootstrap-servers",
+)
 class AsyncApiGeneratorTest : BaseIntegrationTest() {
+
+    companion object {
+        @JvmStatic
+        @DynamicPropertySource
+        fun overrideProps(registry: DynamicPropertyRegistry) {
+            registry.add("spring.kafka.admin.fail-fast") { true }
+            registry.add("spring.cloud.stream.kafka.binder.zkNodes") { "false" }
+        }
+    }
 
     @Autowired
     lateinit var asyncApiService: AsyncApiService
